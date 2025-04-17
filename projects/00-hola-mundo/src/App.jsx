f01185856f21d3605633907ee11e536f72011830
PattoPeraltta <136608427+PattoPeraltta@users.noreply.github.com>
2025-04-17 00:33:14 -0300

    Started proyect

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+export function App () {
+    return (
+        <>
+            <TwitterFollowCard userName = "Rubiu5" name="elrubius"/>
+            <TwitterFollowCard userName = "midudev" name="Miguel Angel Duran"/>
+            <TwitterFollowCard userName = "vegeta777" name="♈️ Vegetta777 ♈️"/>
+        </>
+    )
+}
